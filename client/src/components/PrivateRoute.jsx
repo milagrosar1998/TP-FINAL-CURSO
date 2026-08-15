def679619010080fd042fe6b7d9bcf03bbcbf434
milagrosar1998 <milagrosar1988@gmail.com>
2026-08-15 00:50:40 -0300

    correcciones de rutas y estilos

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -5,42 +5,17 @@ export default function PrivateRoute({ children, rolesPermitidos }) {
   const usuarioGuardado = localStorage.getItem("usuario");
   const token = localStorage.getItem("token");
 
+
   if (!usuarioGuardado || !token) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/login" replace />;
   }
 
-  try {
+  const usuario = JSON.parse(usuarioGuardado);
 
-    const usuario = JSON.parse(usuarioGuardado);
-
-    const payload = JSON.parse(
-      atob(token.split(".")[1])
-    );
-
-    const ahora = Date.now() / 1000;
-
-    
-    if (payload.exp && payload.exp < ahora) {
-
-      localStorage.removeItem("token");
-      localStorage.removeItem("usuario");
-
-      return <Navigate to="/login" />;
-    }
-
-    
-    if (!rolesPermitidos.includes(usuario.rol)) {
-      return <Navigate to="/" />;
-    }
-
-    return children;
-
-  } catch (error) {
-
-    
-    localStorage.removeItem("token");
-    localStorage.removeItem("usuario");
-
-    return <Navigate to="/login" />;
+  
+  if (!rolesPermitidos.includes(usuario.rol)) {
+    return <Navigate to="/" replace />;
   }
+
+  return children;
 }
